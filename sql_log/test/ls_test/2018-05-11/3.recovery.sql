8d41852a3b755f3387e25a27f3c91df18c3b6726
qiuyy128 <qiuyy_128@163.com>
2018-05-14 18:35:21 +0800

    优化弹窗

--- sql_log/test/ls_test/2018-05-11/3.recovery.sql
+++
@@ -1,3 +0,0 @@
-UPDATE ls_yh_jbxx SET sjhm='11111111111', QQ='"333"', GDDH='999', XZQH_DM='110110'  
-WHERE  yhm='firestorm' ;
-
--- sql_log/test/ls_test/2018-05-11/33.recovery.sql
+++
@@ -1,2 +0,0 @@
-UPDATE ls_yh_jbxx SET qq='"333"' where  yhm='firestorm' ;
-
